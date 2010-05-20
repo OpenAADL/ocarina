8925c270883dd917995eae2d5681ca701ab1c813
julien.delange <julien.delange@129961e7-ef38-4bb5-a8f7-c9a525a55882>
2010-05-20 15:47:02 +0000

    * import stub to generate ASN1 data types from AADL models
    git-svn-id: https://tecsw.estec.esa.int/svn/taste/trunk/ocarina@665 129961e7-ef38-4bb5-a8f7-c9a525a55882

---
+++ src/backends/ocarina-backends-asn1_tree-nutils.adb
@@ -0,0 +1,413 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           OCARINA COMPONENTS                             --
+--                                                                          --
+--    O C A R I N A . B A C K E N D S . A S N 1 _ T R E E . N U T I L S     --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                 Copyright (C) 2010, GET-Telecom Paris.                   --
+--                                                                          --
+-- Ocarina  is free software;  you  can  redistribute  it and/or  modify    --
+-- it under terms of the GNU General Public License as published by the     --
+-- Free Software Foundation; either version 2, or (at your option) any      --
+-- later version. Ocarina is distributed  in  the  hope  that it will be    --
+-- useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of  --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General --
+-- Public License for more details. You should have received  a copy of the --
+-- GNU General Public License distributed with Ocarina; see file COPYING.   --
+-- If not, write to the Free Software Foundation, 51 Franklin Street, Fifth --
+-- Floor, Boston, MA 02111-1301, USA.                                       --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable to be   --
+-- covered  by the  GNU  General  Public  License. This exception does not  --
+-- however invalidate  any other reasons why the executable file might be   --
+-- covered by the GNU Public License.                                       --
+--                                                                          --
+--                 Ocarina is maintained by the Ocarina team                --
+--                       (ocarina-users@listes.enst.fr)                     --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with GNAT.Table;
+
+with Charset;   use Charset;
+with Namet;     use Namet;
+with Locations; use Locations;
+
+with Ocarina.Backends;
+
+with Ocarina.ME_AADL.AADL_Instances.Nodes;
+
+use Ocarina.Backends;
+
+package body Ocarina.Backends.ASN1_Tree.Nutils is
+
+   package AIN renames Ocarina.ME_AADL.AADL_Instances.Nodes;
+
+   Initialized : Boolean := False;
+
+   Keyword_Suffix : constant String := "%C";
+   --  Used to mark C keywords and avoid collision with other languages
+
+   type Entity_Stack_Entry is record
+      Current_File    : Node_Id;
+      Current_Entity  : Node_Id;
+   end record;
+
+   No_Depth : constant Int := -1;
+   package Entity_Stack is
+      new GNAT.Table (Entity_Stack_Entry, Int, No_Depth + 1, 10, 10);
+
+   use Entity_Stack;
+
+   procedure New_Operator
+     (O : Operator_Type;
+      I : String := "");
+
+   ------------------------
+   -- Add_Prefix_To_Name --
+   ------------------------
+
+   function Add_Prefix_To_Name
+     (Prefix : String;
+      Name   : Name_Id)
+     return Name_Id
+   is
+   begin
+      Set_Str_To_Name_Buffer (Prefix);
+      Get_Name_String_And_Append (Name);
+      return Name_Find;
+   end Add_Prefix_To_Name;
+
+   ------------------------
+   -- Add_Suffix_To_Name --
+   ------------------------
+
+   function Add_Suffix_To_Name
+     (Suffix : String;
+      Name   : Name_Id)
+     return Name_Id
+   is
+   begin
+      Get_Name_String (Name);
+      Add_Str_To_Name_Buffer (Suffix);
+      return Name_Find;
+   end Add_Suffix_To_Name;
+
+   -----------------------------
+   -- Remove_Suffix_From_Name --
+   -----------------------------
+
+   function Remove_Suffix_From_Name
+     (Suffix : String;
+      Name   : Name_Id)
+     return Name_Id
+   is
+      Length   : Natural;
+      Temp_Str : String (1 .. Suffix'Length);
+   begin
+      Set_Str_To_Name_Buffer (Suffix);
+      Length := Name_Len;
+      Get_Name_String (Name);
+      if Name_Len > Length then
+         Temp_Str := Name_Buffer (Name_Len - Length + 1 .. Name_Len);
+         if Suffix = Temp_Str then
+            Set_Str_To_Name_Buffer (Name_Buffer (1 .. Name_Len - Length));
+            return Name_Find;
+         end if;
+      end if;
+      return Name;
+   end Remove_Suffix_From_Name;
+
+   -------------------------
+   -- Append_Node_To_List --
+   -------------------------
+
+   procedure Append_Node_To_List (E : Node_Id; L : List_Id) is
+      Last : Node_Id;
+
+   begin
+      Last := ASN1_Nodes.Last_Node (L);
+      if No (Last) then
+         ASN1_Nodes.Set_First_Node (L, E);
+      else
+         ASN1_Nodes.Set_Next_Node (Last, E);
+      end if;
+      Last := E;
+      while Present (Last) loop
+         ASN1_Nodes.Set_Last_Node (L, Last);
+         Last := ASN1_Nodes.Next_Node (Last);
+      end loop;
+   end Append_Node_To_List;
+
+   -----------------------
+   -- Insert_After_Node --
+   -----------------------
+
+   procedure Insert_After_Node (E : Node_Id; N : Node_Id) is
+      Next : constant Node_Id := ASN1_Nodes.Next_Node (N);
+   begin
+      ASN1_Nodes.Set_Next_Node (N, E);
+      ASN1_Nodes.Set_Next_Node (E, Next);
+   end Insert_After_Node;
+
+   ------------------------
+   -- Insert_Before_Node --
+   ------------------------
+
+   procedure Insert_Before_Node (E : Node_Id; N : Node_Id; L : List_Id) is
+      Entity : Node_Id;
+   begin
+      Entity := ASN1_Nodes.First_Node (L);
+      if Entity = N then
+         ASN1_Nodes.Set_Next_Node (E, Entity);
+         ASN1_Nodes.Set_First_Node (L, E);
+      else
+         while Present (Entity) loop
+            exit when ASN1_Nodes.Next_Node (Entity) = N;
+            Entity := ASN1_Nodes.Next_Node (Entity);
+         end loop;
+
+         Insert_After_Node (E, Entity);
+      end if;
+   end Insert_Before_Node;
+
+   ---------------
+   -- Copy_Node --
+   ---------------
+
+   function Copy_Node (N : Node_Id) return Node_Id is
+      C : Node_Id;
+   begin
+      case ASN1_Nodes.Kind (N) is
+         when ASN1_Nodes.K_Defining_Identifier =>
+            C := New_Node (ASN1_Nodes.K_Defining_Identifier);
+            ASN1_Nodes.Set_Name (C, ASN1_Nodes.Name (N));
+            ASN1_Nodes.Set_Corresponding_Node
+               (C, ASN1_Nodes.Corresponding_Node (N));
+
+         when others =>
+            raise Program_Error;
+      end case;
+      return C;
+   end Copy_Node;
+
+   -----------
+   -- Image --
+   -----------
+
+   function Image (T : Token_Type) return String is
+      S : String := Token_Type'Image (T);
+   begin
+      To_Lower (S);
+      return S (5 .. S'Last);
+   end Image;
+
+   -----------
+   -- Image --
+   -----------
+
+   function Image (O : Operator_Type) return String is
+      S : String := Operator_Type'Image (O);
+   begin
+      To_Lower (S);
+      for I in S'First .. S'Last loop
+         if S (I) = '_' then
+            S (I) := ' ';
+         end if;
+      end loop;
+      return S (4 .. S'Last);
+   end Image;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize is
+   begin
+      --  Initialize Nutils only once
+
+      if Initialized then
+         return;
+      end if;
+
+      Initialized := True;
+
+      --  Keywords.
+      for I in Keyword_Type loop
+         New_Token (I);
+      end loop;
+
+      --  Graphic Characters
+      New_Token (Tok_And, "&&");
+      New_Token (Tok_Xor, "^");
+      New_Token (Tok_Sharp, "#");
+      New_Token (Tok_Or, "||");
+      New_Token (Tok_Left_Brace, "{");
+      New_Token (Tok_Right_Brace, "}");
+      New_Token (Tok_Mod, "%");
+      New_Token (Tok_Not, "!");
+      New_Token (Tok_Ampersand, "&");
+      New_Token (Tok_Minus, "-");
+      New_Token (Tok_Underscore, "_");
+      New_Token (Tok_Plus, "+");
+      New_Token (Tok_Asterisk, "*");
+      New_Token (Tok_Slash, "/");
+      New_Token (Tok_Dot, ".");
+      New_Token (Tok_Apostrophe, "'");
+      New_Token (Tok_Left_Paren, "(");
+      New_Token (Tok_Right_Paren, ")");
+      New_Token (Tok_Left_Hook, "[");
+      New_Token (Tok_Right_Hook, "]");
+      New_Token (Tok_Comma, ",");
+      New_Token (Tok_Less, "<");
+      New_Token (Tok_Equal, "=");
+      New_Token (Tok_Equal_Equal, "==");
+      New_Token (Tok_Greater, ">");
+      New_Token (Tok_Not_Equal, "!=");
+      New_Token (Tok_Greater_Equal, ">=");
+      New_Token (Tok_Less_Equal, "<=");
+      New_Token (Tok_Colon, ":");
+      New_Token (Tok_Greater_Greater, ">>");
+      New_Token (Tok_Less_Less, "<<");
+      New_Token (Tok_Quote, """");
+      New_Token (Tok_Semicolon, ";");
+      New_Token (Tok_Arrow, "->");
+      New_Token (Tok_Vertical_Bar, "|");
+
+      for O in Op_And .. Op_Or_Else loop
+         New_Operator (O);
+      end loop;
+      New_Operator (Op_And_Symbol, "&");
+      New_Operator (Op_Double_Asterisk, "**");
+      New_Operator (Op_Asterisk, "**");
+      New_Operator (Op_Minus, "-");
+      New_Operator (Op_Plus, "+");
+      New_Operator (Op_Asterisk, "*");
+      New_Operator (Op_Slash, "/");
+      New_Operator (Op_Less, "<");
+      New_Operator (Op_Equal, "=");
+      New_Operator (Op_Equal_Equal, "==");
+      New_Operator (Op_Greater, ">");
+      New_Operator (Op_Not_Equal, "!=");
+      New_Operator (Op_Greater_Equal, ">=");
+      New_Operator (Op_Less_Equal, "<=");
+      New_Operator (Op_Greater_Greater, ">>");
+      New_Operator (Op_Less_Less, "<<");
+      New_Operator (Op_Semicolon, ";");
+      New_Operator (Op_Arrow, "=>");
+      New_Operator (Op_Vertical_Bar, "|");
+   end Initialize;
+
+   -----------
+   -- Reset --
+   -----------
+
+   procedure Reset is
+   begin
+      Entity_Stack.Init;
+
+      Initialized := False;
+   end Reset;
+
+   --------------
+   -- Is_Empty --
+   --------------
+
+   function Is_Empty (L : List_Id) return Boolean is
+   begin
+      return L = No_List or else No (ASN1_Nodes.First_Node (L));
+   end Is_Empty;
+
+   ------------
+   -- Length --
+   ------------
+
+   function Length (L : List_Id) return Natural is
+      N : Node_Id;
+      C : Natural := 0;
+   begin
+      if not Is_Empty (L) then
+         N := ASN1_Nodes.First_Node (L);
+
+         while Present (N) loop
+            C := C + 1;
+            N := ASN1_Nodes.Next_Node (N);
+         end loop;
+      end if;
+
+      return C;
+   end Length;
+
+   --------------
+   -- New_Node --
+   --------------
+
+   function New_Node
+     (Kind : ASN1_Nodes.Node_Kind;
+      From : Node_Id := No_Node)
+     return Node_Id
+   is
+      N : Node_Id;
+   begin
+      ASN1_Nodes.Entries.Increment_Last;
+      N := ASN1_Nodes.Entries.Last;
+      ASN1_Nodes.Entries.Table (N) := ASN1_Nodes.Default_Node;
+      ASN1_Nodes.Set_Kind (N, Kind);
+
+      if Present (From) then
+         ASN1_Nodes.Set_Loc (N, AIN.Loc (From));
+      else
+         ASN1_Nodes.Set_Loc (N, No_Location);
+      end if;
+
+      return N;
+   end New_Node;
+
+   ---------------
+   -- New_Token --
+   ---------------
+
+   procedure New_Token
+     (T : Token_Type;
+      I : String := "")
+   is
+      Name : Name_Id;
+   begin
+      if T in Keyword_Type then
+         --  Marking the token image as a keyword for fas searching
+         --  purpose, we add the prefix to avoir collision with other
+         --  languages keywords
+
+         Set_Str_To_Name_Buffer (Image (T));
+         Name := Name_Find;
+         Name := Add_Suffix_To_Name (Keyword_Suffix, Name);
+         Set_Name_Table_Byte (Name, Types.Byte (Token_Type'Pos (T) + 1));
+
+         Set_Str_To_Name_Buffer (Image (T));
+      else
+         Set_Str_To_Name_Buffer (I);
+      end if;
+      Token_Image (T) := Name_Find;
+   end New_Token;
+
+   ------------------
+   -- New_Operator --
+   ------------------
+
+   procedure New_Operator
+     (O : Operator_Type;
+      I : String := "") is
+   begin
+      if O in Keyword_Operator then
+         Set_Str_To_Name_Buffer (Image (O));
+      else
+         Set_Str_To_Name_Buffer (I);
+      end if;
+      Operator_Image (Operator_Type'Pos (O)) := Name_Find;
+   end New_Operator;
+
+end Ocarina.Backends.ASN1_Tree.Nutils;
---
+++ src/backends/ocarina-backends-asn1_tree-nutils.ads
@@ -0,0 +1,159 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           OCARINA COMPONENTS                             --
+--                                                                          --
+--    O C A R I N A . B A C K E N D S . A S N 1 _ T R E E . N U T I L S     --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--                 Copyright (C) 2010, GET-Telecom Paris.                   --
+--                                                                          --
+-- Ocarina  is free software;  you  can  redistribute  it and/or  modify    --
+-- it under terms of the GNU General Public License as published by the     --
+-- Free Software Foundation; either version 2, or (at your option) any      --
+-- later version. Ocarina is distributed  in  the  hope  that it will be    --
+-- useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of  --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General --
+-- Public License for more details. You should have received  a copy of the --
+-- GNU General Public License distributed with Ocarina; see file COPYING.   --
+-- If not, write to the Free Software Foundation, 51 Franklin Street, Fifth --
+-- Floor, Boston, MA 02111-1301, USA.                                       --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable to be   --
+-- covered  by the  GNU  General  Public  License. This exception does not  --
+-- however invalidate  any other reasons why the executable file might be   --
+-- covered by the GNU Public License.                                       --
+--                                                                          --
+--                 Ocarina is maintained by the Ocarina team                --
+--                       (ocarina-users@listes.enst.fr)                     --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Ocarina.Backends.ASN1_Tree.Nodes;
+
+package Ocarina.Backends.ASN1_Tree.Nutils is
+
+   package ASN1_Nodes renames Ocarina.Backends.ASN1_Tree.Nodes;
+
+   type Token_Type is
+     (
+      --   Token name      Token type
+      --   Keywords
+      Tok_Null,            -- NULL   **** First Keyword
+      Tok_Module,          -- MODULE
+
+      --  Graphic Characters
+      Tok_Xor,             -- ^
+      Tok_Sharp,           -- #
+      Tok_Mod,             -- %
+      Tok_Not,             -- !
+      Tok_Left_Brace,      -- {
+      Tok_Right_Brace,     -- }
+      Tok_Or,              -- ||
+      Tok_And,             -- &&
+      Tok_Ampersand,       -- &
+      Tok_Minus,           -- -
+      Tok_Underscore,      -- _
+      Tok_Plus,            -- +
+      Tok_Asterisk,        -- *
+      Tok_Slash,           -- /
+      Tok_Quote,           -- "
+      Tok_Dot,             -- .
+      Tok_Apostrophe,      -- '
+      Tok_Left_Paren,      -- (
+      Tok_Right_Paren,     -- )
+      Tok_Left_Hook,       -- [
+      Tok_Right_Hook,      -- ]
+      Tok_Comma,           -- ,
+      Tok_Less,            -- <
+      Tok_Equal,           -- =
+      Tok_Equal_Equal,     -- ==
+      Tok_Greater,         -- >
+      Tok_Not_Equal,       -- /=
+      Tok_Greater_Equal,   -- >=
+      Tok_Less_Equal,      -- <=
+      Tok_Colon,           -- :
+      Tok_Greater_Greater, -- >>
+      Tok_Less_Less,       -- <<
+      Tok_Semicolon,       -- ;
+      Tok_Arrow,           -- ->
+      Tok_Vertical_Bar);   -- |
+
+   Token_Image : array (Token_Type) of Name_Id;
+
+   subtype Keyword_Type is Token_Type
+     range Tok_Null .. Tok_Module;
+
+   type Operator_Type  is
+     (Op_Not,             -- not
+      Op_And,             -- and
+      Op_In,              -- in
+      Op_And_Then,        -- and then
+      Op_Or,              -- or
+      Op_Or_Else,         -- or else
+      Op_And_Symbol,      -- &
+      Op_Double_Asterisk, -- **
+      Op_Minus,           -- -
+      Op_Plus,            -- +
+      Op_Asterisk,        -- *
+      Op_Slash,           -- /
+      Op_Less,            -- <
+      Op_Equal,           -- =
+      Op_Equal_Equal,     -- ==
+      Op_Greater,         -- >
+      Op_Not_Equal,       -- !=
+      Op_Greater_Equal,   -- >=
+      Op_Less_Equal,      -- <=
+      Op_Greater_Greater, -- >>
+      Op_Less_Less,       -- <<
+      Op_Semicolon,       -- ;
+      Op_Arrow,           -- ->
+      Op_Vertical_Bar,    -- |
+      Op_None);           -- No operation
+
+   Operator_Image : array
+     (Operator_Type'Pos (Op_And) ..  Operator_Type'Pos (Op_Vertical_Bar))
+     of Name_Id;
+
+   subtype Keyword_Operator is Operator_Type
+     range Operator_Type'First .. Op_Or_Else;
+
+   function Length (L : List_Id) return Natural;
+
+   function Is_Empty (L : List_Id) return Boolean;
+
+   procedure Reset;
+
+   procedure Initialize;
+
+   function Image (O : Operator_Type) return String;
+
+   function Image (T : Token_Type) return String;
+
+   function Copy_Node (N : Node_Id) return Node_Id;
+
+   procedure Insert_Before_Node (E : Node_Id; N : Node_Id; L : List_Id);
+
+   procedure Insert_After_Node (E : Node_Id; N : Node_Id);
+
+   procedure Append_Node_To_List (E : Node_Id; L : List_Id);
+
+   function Remove_Suffix_From_Name
+     (Suffix : String; Name   : Name_Id) return Name_Id;
+
+   function Add_Suffix_To_Name
+     (Suffix : String; Name   : Name_Id) return Name_Id;
+
+   function Add_Prefix_To_Name
+     (Prefix : String; Name   : Name_Id) return Name_Id;
+
+   function New_Node
+     (Kind : ASN1_Nodes.Node_Kind;
+      From : Node_Id := No_Node)
+     return Node_Id;
+
+   procedure New_Token (T : Token_Type; I : String := "");
+
+end Ocarina.Backends.ASN1_Tree.Nutils;
