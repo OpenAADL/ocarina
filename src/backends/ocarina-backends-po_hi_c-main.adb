9f9b54a0f7f25a85a9df70a63e2c38ed59f865a4
julien.delange <julien.delange@129961e7-ef38-4bb5-a8f7-c9a525a55882>
2011-02-11 09:51:20 +0000

    * ocarina/src/backends/ocarina-backends-po_hi_c-main.adb    - avoid warning for initialization functions
     * ocarina/resources/AADLv2/components/devices/rasta-spacewire.aadl
       ocarina/resources/AADLv2/components/devices.aadl
       - Add the specification of the spacewire configuration type
    
    
    
    git-svn-id: https://tecsw.estec.esa.int/svn/taste/trunk/ocarina@2109 129961e7-ef38-4bb5-a8f7-c9a525a55882

--- src/backends/ocarina-backends-po_hi_c-main.adb
+++ src/backends/ocarina-backends-po_hi_c-main.adb
@@ -306,6 +306,16 @@ package body Ocarina.Backends.PO_HI_C.Main is
                      (Corresponding_Instance (C)) /= No_Node
                then
                   Append_Node_To_List
+                     (Make_Extern_Entity_Declaration
+                        (Make_Function_Specification
+                              (Map_C_Subprogram_Identifier
+                                 (Get_Thread_Initialize_Entrypoint
+                                    (Corresponding_Instance (C))),
+                           Parameters   => No_List,
+                           Return_Type  => New_Node (CTN.K_Void))),
+                     CTN.Declarations (Current_File));
+
+                  Append_Node_To_List
                      (Make_Call_Profile
                         (Map_C_Subprogram_Identifier
                            (Get_Thread_Initialize_Entrypoint
