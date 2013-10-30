988680f64f9860cc871f8ab453a28b3120932dc8
yoogx <hugues.jerome@gmail.com>
2013-10-30 13:02:24 +0100

    * Add Python_Shell as option

--- src/core/mgmt/ocarina-options.ads
+++ src/core/mgmt/ocarina-options.ads
@@ -48,7 +48,8 @@ package Ocarina.Options is
       Show_Usage,
       Parse_Scenario_Files_First,
       Analyze_With_Cheddar,
-      Shell);
+      Shell,
+      Python_Shell);
 
    Root_System_Name             : Name_Id := No_Name;
    Installation_Directory       : Name_Id := No_Name;
