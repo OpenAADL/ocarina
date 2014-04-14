e4ed68a095944e2b704c969a256e9814a5351f3a
yoogx <hugues.jerome@gmail.com>
2014-04-14 16:49:38 -0700

    * Add testsuite for Python

---
+++ tests/python/test_ocarina.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+from ocarina import *;
+
+def main ():
+    '''Test function'''
+
+    for backends in Backends:
+        print(backends);
+
+    load("rma.aadl");             # load a file
+    load("deployment.aadl");      # load a file
+    analyze();                    # analyze models
+    instantiate("rma.erc32");     # instantiate
+    generate (Backends.polyorb_hi_ada);
+
+if __name__ == "__main__":
+    main ()
+    sys.exit (0);                         # exit
+
+################################################################################
+
