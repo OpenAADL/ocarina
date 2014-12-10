732e59ea89fa94cd7c221f2d150b89f02cbb5cbe
yoogx <hugues.jerome@gmail.com>
2014-12-10 21:13:41 +0100

    * Simple test

--- src/backends/ocarina-backends-deos_conf-partitions.adb
+++ src/backends/ocarina-backends-deos_conf-partitions.adb
@@ -1,3 +1,44 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           OCARINA COMPONENTS                             --
+--                                                                          --
+--                  OCARINA.BACKENDS.DEOS_CONF.PARTITIONS                   --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--                     Copyright (C) 2014 ESA & ISAE.                       --
+--                                                                          --
+-- Ocarina  is free software;  you  can  redistribute  it and/or  modify    --
+-- it under terms of the GNU General Public License as published by the     --
+-- Free Software Foundation; either version 2, or (at your option) any      --
+-- later version. Ocarina is distributed  in  the  hope  that it will be    --
+-- useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of  --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General --
+-- Public License for more details. You should have received  a copy of the --
+-- GNU General Public License distributed with Ocarina; see file COPYING.   --
+-- If not, write to the Free Software Foundation, 51 Franklin Street, Fifth --
+-- Floor, Boston, MA 02111-1301, USA.                                       --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable to be   --
+-- covered  by the  GNU  General  Public  License. This exception does not  --
+-- however invalidate  any other reasons why the executable file might be   --
+-- covered by the GNU Public License.                                       --
+--                                                                          --
+--                 Ocarina is maintained by the TASTE project               --
+--                      (taste-users@lists.tuxfamily.org)                   --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Text_IO; use Ada.Text_IO;
+with Ocarina.Namet; use Ocarina.Namet;
+
+with Ocarina.ME_AADL.AADL_Instances.Debug;
+use Ocarina.ME_AADL.AADL_Instances.Debug;
+with Ocarina.Backends.Properties.ARINC653;
+use Ocarina.Backends.Properties.ARINC653;
+
 --  with Locations;
 with Ocarina.ME_AADL;
 with Ocarina.ME_AADL.AADL_Instances.Nodes;
@@ -214,6 +255,22 @@ package body Ocarina.Backends.Deos_Conf.Partitions is
       Push_Entity (U);
 
       Partition_Identifier := 1;
+
+      declare
+         Module_Schedule : constant Schedule_Window_Record_Term_Array
+           := Get_Module_Schedule_Property (E);
+      begin
+         for J in Module_Schedule'Range loop
+            Put_Line ("Module Schedule slot #" & J'Img);
+            Put_Line (" -> "
+                        & Get_Name_String (Module_Schedule (J).Partition));
+            Put_Line (" -> "
+                        & Module_Schedule (J).Duration.T'Img
+                        & " " & Module_Schedule (J).Duration.U'Img);
+            Put_Line (" -> "
+                        & Module_Schedule (J).Periodic_Processing_Start'Img);
+         end loop;
+      end;
 
       Current_XML_Node := XTN.Root_Node (XTN.XML_File (U));
 
