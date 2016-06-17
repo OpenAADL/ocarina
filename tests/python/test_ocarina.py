9c238e0a869d39acc6cba01eb04a9f0276349f84
Jerome Hugues <hugues.jerome@gmail.com>
2016-06-17 22:02:49 +0200

    * Update refernce outputs

--- tests/python/test_ocarina.py
+++ tests/python/test_ocarina.py
@@ -5,24 +5,24 @@ from ocarina import *;
 def main ():
     '''Test function'''
 
-    for backends in Backends:
-        print(backends);
+    result = add_real_library("rma.real")      # load a file
+    print "%r" % (result)
+    result = load("rma.aadl")             # load a file
+    print "%r" % (result)
+    result = load("deployment.aadl")      # load a file
+    print "%r" % (result)
+    result = analyze()                    # analyze models
+    print "Analysis result %r" % (result)
 
-    load("rma.aadl");             # load a file
-    load("deployment.aadl");      # load a file
-    result = analyze();                    # analyze models
-    print "Analysis result %r" % (result);
-    instantiate("rma.erc32");     # instantiate
-    generate (Backends.polyorb_hi_ada);
+    result = set_real_theorem ("rma_prop_def")
+    result = generate (Backends.real_theorem)
+    print "REAL execution %r" % (result)
+    print ""
 
-    reset();
-
-    load("rma.aadl");             # load a file
-    load("deployment.aadl");      # load a file
-    analyze();                    # analyze models
-    instantiate("rma.erc32");     # instantiate
-    generate (Backends.polyorb_hi_ada);
+    result = set_real_theorem ("rma_prop_def_2")
+    result = generate (Backends.real_theorem)
+    print "REAL execution %r" % (result)
 
 if __name__ == "__main__":
     main ()
-    sys.exit (0);                         # exit
+    sys.exit (0)                         # exit
