6982b504ad7832f8054f640f132753ef32200d3d
Bechir Zalila <bechir@zalila.net>
2016-07-09 11:48:56 +0100

    * (ocarina-property_sets.ad?): Mark ErrorLibrary as a standard         property set
    	(By Fatma Kacem)

--- src/core/tree/ocarina-property_sets.adb
+++ src/core/tree/ocarina-property_sets.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---    Copyright (C) 2008-2009 Telecom ParisTech, 2010-2015 ESA & ISAE.      --
+--    Copyright (C) 2008-2009 Telecom ParisTech, 2010-2016 ESA & ISAE.      --
 --                                                                          --
 -- Ocarina  is free software; you can redistribute it and/or modify under   --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -47,7 +47,8 @@ package body Ocarina.Property_Sets is
       S_Memory_Properties,
       S_Modeling_Properties,
       S_Timing_Properties,
-      S_Programming_Properties);
+      S_Programming_Properties,
+      S_ErrorLibrary);
 
    Standard_Property_Sets_Table : constant array
    (AADL_Version_Type) of Standard_Property_Set_Array_Access :=
