c7da652598d0cbba39711bb10fba8ed2e7364f99
yoogx <hugues.jerome@gmail.com>
2017-03-21 14:37:40 +0100

    * Filter out polyorb-hi-c subdir

--- src/backends/ocarina-backends-build_utils.adb
+++ src/backends/ocarina-backends-build_utils.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---    Copyright (C) 2008-2009 Telecom ParisTech, 2010-2016 ESA & ISAE.      --
+--    Copyright (C) 2008-2009 Telecom ParisTech, 2010-2017 ESA & ISAE.      --
 --                                                                          --
 -- Ocarina  is free software; you can redistribute it and/or modify under   --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -1423,7 +1423,9 @@ package body Ocarina.Backends.Build_Utils is
                Write_Line
                  ("###################################################");
                Write_Eol;
-               Write_Str ("SUBDIRS = $(filter-out Makefile, $(wildcard *))");
+               Write_Str
+                 ("SUBDIRS = " &
+                    "$(filter-out Makefile polyorb-hi-c, $(wildcard *))");
                Write_Eol;
                Write_Line ("all:");
                Write_Line
